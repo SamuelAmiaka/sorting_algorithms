4e312432de8685b9a23eb70b53a687a0ee9b2a22
SamuelAmiaka <amiakasamuel010@gmail.com>
2022-11-07 13:14:19 -0800

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,25 @@
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-// BUBBLE SORT
-//------------------------------------------------------- 
-
-void bubbleSort(vector <int> &v, int n) {
-	bool swapped = true;
-	int i = 0;
-
-	while (i < n - 1 && swapped) { // keep going while we swap in the unordered part
-		swapped = false;
-		
-		for (int j = n - 1; j > i; j--) { // unordered part
-			
-			if (v[j] < v[j - 1]) {
-				swap(v[j], v[j - 1]);
-				swapped = true;
-			}
-		}
-		i++;
-	}
+void bubbleSort(int arr[], int n){
+    int flag;
+    for(int i=0; i<n-1; i++){
+        flag=0;
+        for(int j=0; j<n-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag=1;
+            }
+        }
+        if(flag==0)break;
+    }
+}
+int main()
+{
+    int arr[]={3,5,27,8};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
---
+++ sort.h
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+using namespace std;
+
+void bubbleSort(int arr[], int n);
+void insertion_sort_list(listint_t **list);
+
+#endif
